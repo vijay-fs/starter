3fa56149fb33c0cde10a16a87654a5e85795eadb
vijay-fs <vijaykarthik9868@gmail.com>
2024-12-24 18:20:04 +0530

    form validation intro

--- views/note.view.php
+++ views/note.view.php
@@ -5,7 +5,7 @@
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       <a class="mb-6 block text-sm font-semibold leading-6 hover:underline text-blue-500" href="/notes">Go Back</a>
-     <p><?= $note["body"]; ?></p>
+     <p><?= htmlspecialchars($note['body']) ?></p>
     </div>
   </main>
  <?php require("partials/footer.php"); ?>
--- views/notes.view.php
+++ views/notes.view.php
@@ -9,7 +9,7 @@
      <?php foreach($notes as $note): ?>
       <li>
         <a class="hover:underline text-blue-500" href="/note?id=<?= $note["id"]; ?>">
-          <?= $note["body"]; ?>
+          <?= htmlspecialchars($note['body']) ?>
         </a>
       </li>
       <?php endforeach; ?>
