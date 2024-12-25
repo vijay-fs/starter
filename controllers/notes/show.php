3c1641d48059c91d27937197705ee0e1226e0adf
vijay-fs <vijaykarthik9868@gmail.com>
2024-12-25 19:55:44 +0530

    Naming conventions

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -11,4 +11,4 @@ $note = $db->query('select * from notes where id = :id', [
     'id' => $_GET['id']
 ])->findOrFail();
 authorize($note['user_id'] === $currentUserId);
-require "views/note.view.php";
+require "views/notes/show.view.php";
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -1,11 +1,11 @@
 
-   <?php require("partials/head.php"); ?>
-  <?php require("partials/nav.php"); ?>
-  <?php require("partials/banner.php"); ?>
+   <?php require("views/partials/head.php"); ?>
+  <?php require("views/partials/nav.php"); ?>
+  <?php require("views/partials/banner.php"); ?>
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       <a class="mb-6 block text-sm font-semibold leading-6 hover:underline text-blue-500" href="/notes">Go Back</a>
      <p><?= htmlspecialchars($note['body']) ?></p>
     </div>
   </main>
- <?php require("partials/footer.php"); ?>
+ <?php require("views/partials/footer.php"); ?>
