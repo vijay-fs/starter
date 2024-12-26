d558e6abbf8a05147a4f47bfbe00547867a9a48d
vijay-fs <vijaykarthik9868@gmail.com>
2024-12-26 12:34:50 +0530

    password hashing

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -26,7 +26,7 @@ if ($user) {
 } else {
     $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
         'email' => $email,
-        'password' => $password // NEVER store database passwords in clear text. We'll fix this in the login form episode. :)
+        'password' => password_hash($password, PASSWORD_BCRYPT)
     ]);
     $_SESSION['user'] = [
         'email' => $email
--- views/index.view.php
+++ views/index.view.php
@@ -5,9 +5,7 @@
 
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900"> 
-        Home
-      </h1>
+     <p>Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?>. Welcome to the home page.</p>
     </div>
   </main>
  <?php require("partials/footer.php"); ?>
