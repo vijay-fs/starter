feaf32af3cb0c92c9fed5198b645f9a8e0f2bcf2
vijay-fs <vijaykarthik9868@gmail.com>
2024-12-26 10:51:59 +0530

    One request one controller

--- controllers/notes/index.php
+++ controllers/notes/index.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
+
 use Core\Database;
+
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 
-$heading = "Notes";
-
-$notes = $db->query("SELECT * FROM notes where user_id = 1")->get();
+$notes = $db->query('select * from notes where user_id = 1')->get();
 
 view("notes/index.view.php", [
     'heading' => 'My Notes',
